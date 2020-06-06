5e026170cc6f896e6af79f797c76d139bda0e3f1
David Cristóbal <david@juanked.es>
2020-06-07 01:01:09 +0200

    Algunos cambios

--- javaAFS.2020/afs/ViceReader.java
+++ javaAFS.2020/afs/ViceReader.java
@@ -1,11 +1,14 @@
 // Interfaz de servidor que define los métodos remotos
 // para completar la descarga de un fichero
 package afs;
+
 import java.rmi.*;
+import java.io.*;
 
 public interface ViceReader extends Remote {
     public byte[] read(int tam) throws RemoteException;
     public void close() throws RemoteException;
     /* añada los métodos remotos que requiera */
+    // public ViceReaderImpl(String fileName, char[] modoEjec) throws RemoteException;
 }       
 
--- javaAFS.2020/afs/ViceReaderImpl.java
+++ javaAFS.2020/afs/ViceReaderImpl.java
@@ -1,20 +1,43 @@
 // Implementación de la interfaz de servidor que define los métodos remotos
 // para completar la descarga de un fichero
 package afs;
+
 import java.rmi.*;
 import java.rmi.server.*;
+import java.io.*;
 
 public class ViceReaderImpl extends UnicastRemoteObject implements ViceReader {
     private static final String AFSDir = "AFSDir/";
+    private RandomAccessFile lector;
 
-    public ViceReaderImpl(String fileName /* añada los parámetros que requiera */)
-		    throws RemoteException {
+    public ViceReaderImpl(String fileName, String modoEjec) throws RemoteException {
+        try {
+            String directorio = AFSDir + fileName;
+            this.lector = new RandomAccessFile(directorio, modoEjec);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
+
     public byte[] read(int tam) throws RemoteException {
-        return null;
+        try {
+            byte[] b = new byte[tam];
+            for (int i = 0; i < b.length; i++) {
+                int leido = lector.read(b);
+                
+            }
+            
+        } catch (IOException e) {
+            //TODO: handle exception
+        }
+
+        return b;
     }
+
     public void close() throws RemoteException {
         return;
     }
-}       
-
+}
